7f5a9f8b8ee0948f56c97b9fc2f741cdfaf2e0a8
pyrhaa <sboubtane@protonmail.com>
2022-05-27 12:58:57 +0200

    all install ok

--- unicafe-redux/src/reducer.js
+++ unicafe-redux/src/reducer.js
@@ -2,22 +2,22 @@ const initialState = {
   good: 0,
   ok: 0,
   bad: 0
-}
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
     case 'GOOD':
-      return state
+      return state;
     case 'OK':
-      return state
+      return state;
     case 'BAD':
-      return state
+      return state;
     case 'ZERO':
-      return state
-    default: return state
+      return state;
+    default:
+      return state;
   }
-  
-}
+};
 
-export default counterReducer
+export default counterReducer;
