ba5193b6924b166ce85b7664b31743cda3268501
pyrhaa <sboubtane@protonmail.com>
2022-05-29 11:37:17 +0200

    implement other dispatch

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import reducer from './reducer'
+import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import reducer from './reducer';
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
   const good = () => {
     store.dispatch({
       type: 'GOOD'
-    })
-  }
+    });
+  };
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    });
+  };
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    });
+  };
+  const reset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    });
+  };
 
   return (
     <div>
@@ -22,13 +37,13 @@ const App = () => {
       <div>ok</div>
       <div>bad</div>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
