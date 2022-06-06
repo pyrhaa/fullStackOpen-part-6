1c9a67522d874e6e3f2200894ae87bcd5f2e24c1
pyrhaa <sboubtane@protonmail.com>
2022-06-06 16:10:50 +0200

    avoid error in useSelector of anecdotelist

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { voteOf } from '../reducers/anecdoteReducer';
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) => state.anecdotes);
   const dispatch = useDispatch();
 
   const vote = (id) => {
--- redux-anecdotes/src/store.js
+++ redux-anecdotes/src/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import reducer from './reducers/anecdoteReducer';
+import anecdoteReducer from './reducers/anecdoteReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = configureStore(reducer, composeWithDevTools());
+const store = configureStore(
+  {
+    reducer: {
+      anecdotes: anecdoteReducer
+    }
+  },
+  composeWithDevTools()
+);
+
+export default store;
