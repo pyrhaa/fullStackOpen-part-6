b3f6cee6dfa9b9d1b0d643d0ce84fb43561e904d
pyrhaa <sboubtane@protonmail.com>
2022-06-10 16:41:14 +0200

    6.11 step9 ok

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -1,13 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { voteOf } from '../reducers/anecdoteReducer';
+import { notifChange } from '../reducers/notifReducer';
 
 //I does a .slice() before the .sort() because we need to dont mutate the array so we do a copy like this.
 const AnecdoteList = () => {
   const anecdotes = useSelector((state) => state.anecdotes);
   const dispatch = useDispatch();
 
-  const vote = (id) => {
-    dispatch(voteOf(id));
+  const vote = (arg) => {
+    const data = arg;
+    dispatch(voteOf(data.id));
+    dispatch(notifChange(`You voted for <${data.content}>`, 5));
   };
 
   return (
@@ -20,7 +23,12 @@ const AnecdoteList = () => {
             <div>{anecdote.content}</div>
             <div>
               has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
+              <button
+                onClick={() => {
+                  vote(anecdote);
+                }}>
+                vote
+              </button>
             </div>
           </div>
         ))}
