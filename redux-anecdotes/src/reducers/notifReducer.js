e5c97cda0b6cc1adf7ca1d09fb13a91ed2ad31e9
pyrhaa <sboubtane@protonmail.com>
2022-06-10 14:51:51 +0200

    createslice even for notif reducer

--- redux-anecdotes/src/reducers/notifReducer.js
+++ redux-anecdotes/src/reducers/notifReducer.js
@@ -1,26 +1,41 @@
-const notifReducer = (state = null, action) => {
-  switch (action.type) {
-    case 'SET_NOTIF':
-      return action.notif;
-    default:
-      return state;
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = null;
+
+const notifSlice = createSlice({
+  name: 'notif',
+  initialState,
+  reducers: {
+    notifChange(state, action) {}
   }
-};
+});
 
-export const notifChange = (notif, time) => {
-  return async (dispatch) => {
-    dispatch({
-      type: 'SET_NOTIF',
-      notif
-    });
+/** without createSlice */
+// const notifReducer = (state = null, action) => {
+//   switch (action.type) {
+//     case 'SET_NOTIF':
+//       return action.notif;
+//     default:
+//       return state;
+//   }
+// };
 
-    setTimeout(() => {
-      dispatch({
-        type: 'SET_NOTIF',
-        notif: null
-      });
-    }, time * 1000);
-  };
-};
+// export const notifChange = (notif, time) => {
+//   return async (dispatch) => {
+//     dispatch({
+//       type: 'SET_NOTIF',
+//       notif
+//     });
 
-export default notifReducer;
+//     setTimeout(() => {
+//       dispatch({
+//         type: 'SET_NOTIF',
+//         notif: null
+//       });
+//     }, time * 1000);
+//   };
+// };
+
+// export default notifReducer;
+export const { notifChange } = notifSlice.actions;
+export default notifSlice.reducer;
